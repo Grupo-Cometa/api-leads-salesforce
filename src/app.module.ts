902298b3230492fc0cf7f34bb0bbc93c3553306d
Vinicius C C <76782647+MasterGoodbytes@users.noreply.github.com>
2024-08-05 10:39:54 -0400

    Remoção de imports desnecessários e reativação de validação middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,14 +18,10 @@ import { QuoteModule } from './core/quote/quote.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule  {
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer.apply(apiKeyMiddleware).forRoutes('*');
-  // }
-//############################################# ORIGINAL:
-//   export class AppModule implements NestModule {
-//     configure(consumer: MiddlewareConsumer) {
-//       consumer.apply(apiKeyMiddleware).forRoutes('*');
-//     }
+
+  export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+      consumer.apply(apiKeyMiddleware).forRoutes('*');
+    }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { json, urlencoded } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swaggerAPI.json';
 
