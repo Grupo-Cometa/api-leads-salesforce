f1dcbc56d3d8d287778b1eaf9b9c325b3bb41a13
Vinicius C C <vinicaparroz@gmail.com>
2024-08-26 11:06:25 -0400

    erro 401 quando autorizado arrumado (mudei x-api-key para x-api-token)

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swaggerAPI.json';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const options = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+  };
+
+  app.enableCors(options);
+
 
   app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
